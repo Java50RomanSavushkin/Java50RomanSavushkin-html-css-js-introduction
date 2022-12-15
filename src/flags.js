34eeea1ed1b880eb3c2bb260e3df2fb8d1641fdc
RomanSavushkin <romarsa82@gmail.com>
2022-12-15 21:44:19 +0200

    HW-09

--- src/flags.js
+++ src/flags.js
@@ -1,13 +1,22 @@
 const detailsImage = document.querySelector(".details-image");
 const detailsTitle = document.querySelector(".details-title");
 const anchorElements = document.querySelectorAll(".thumbnails-anchor");
+const mainElement = document.querySelector(".main-class")
+const HIDDEN = "hidden";
+function showDitails() {
+    mainElement.classList.remove(HIDDEN);
+}
+function hideDetails() {
+    mainElement.classList.add(HIDDEN)
+}
 function setDetails(anchor) {
-    const dataImage = anchor.getAttribute("data-details-image");
     detailsImage.src = anchor.getAttribute("data-details-image");
     detailsTitle.innerHTML = anchor.getAttribute("data-details-title");
+    showDitails();
 }
 for (let i = 0; i < anchorElements.length; i++) {
     anchorElements[i].addEventListener("click", function () {
         setDetails(anchorElements[i]);
     })
 }
+hideButtonElement.addEventListener("click", hideDetails);
