37fb5418e6b52c6b74e7dee1da3310dc7215e704
RomanSavushkin <romarsa82@gmail.com>
2022-12-17 00:38:02 +0200

    audio_test

--- src/flags.js
+++ src/flags.js
@@ -1,29 +1,41 @@
 const detailsImage = document.querySelector(".details-image");
 const detailsTitle = document.querySelector(".details-title");
+const detailsContainer = document.querySelector(".details-container");
 const anchorElements = document.querySelectorAll(".thumbnails-anchor");
 const mainElement = document.querySelector(".main-class")
+const hideButtonElement = document.getElementById("hide-button");
 const HIDDEN = "hidden";
-const detailsContainer = document.querySelector(".details-container");
 const IS_POINT = "is-point";
+const audio = document.getElementById("myAudio");
 
-function showDitails() {
+function showDetails() {
     mainElement.classList.remove(HIDDEN);
     detailsContainer.classList.add(IS_POINT);
-    setTimeout(function(){
+    setTimeout(function () {
         detailsContainer.classList.remove(IS_POINT);
     })
 }
+
 function hideDetails() {
     mainElement.classList.add(HIDDEN)
+    audio.pause();
+    audio.currentTime = 0;
 }
+
 function setDetails(anchor) {
-    detailsImage.src = anchor.getAttribute("data-details-image");
+    const dataImage = anchor.getAttribute("data-details-image");
+    const dataAudio = anchor.getAttribute("data-details-audio");
+    detailsImage.src = dataImage;
+    showDetails();
     detailsTitle.innerHTML = anchor.getAttribute("data-details-title");
-    showDitails();
+    audio.src = dataAudio
 }
+
 for (let i = 0; i < anchorElements.length; i++) {
     anchorElements[i].addEventListener("click", function () {
         setDetails(anchorElements[i]);
     })
 }
-// hideButtonElement.addEventListener("click", hideDetails);
+
+hideButtonElement.addEventListener("click", hideDetails);
+
