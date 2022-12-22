f44cd9d7249d4b32f307c26896ccf4c7b4740a07
RomanSavushkin <romarsa82@gmail.com>
2022-12-22 22:30:56 +0200

    HW-11

--- app.js
+++ app.js
@@ -1,91 +1,52 @@
-//number
-// let num = 25.325;
-// num = 40.3;
-// num = Math.floor(num);
-// num = Math.trunc(num);
-// num = Math.ceil(num);
-// num = Math.round(num);
-// num **= 2; // num = num ** 2
-
-//strings
-
-// let str = "a'bc'd";
-// let str1 = 'a"bc"d';
-// let res = "123" + 56;
-// let res1 = "123" - 56;
-// let res2 = Math.round("123" / 56);
-// let res3 = "123" / "2";
-// let res4 = "ab" / 8;
-
-//boolean
-// let a;
-// let error = true;
-// let res = true + true;
-// let res1 = true && false;
-// let res2 = -123 && 256;
-// let message = error && "some error happenning";
-// let res3 = a || 10;
-// let res4 = "123" > "9";
-// let res5 = "123" > 9;
 // HW #11
-// function getDigitsSum(number) {
-//     //computes sum of all digits in the number
-//     //number may be either negative or positive or 0
-// }
 
-
-// function getDigitsSum(number) {
-//     let sum = 0;
-//     while (number != 0) {
-//         let rem = number % 10;
-//         number = Math.floor(number / 10);
-//         sum = sum + rem;
-//     }
-//     return sum;
-// }
-// console.log(getDigitsSum(122))
-
-
-// function computeExpression(expressionString) {
-//example of expression "9000 / ((10+20) ** 2)"
-//returns 10
-//task for searching in Internet
-//only one line code
-//how to use Internet for finding required code
-// }
-//console.log (computeExpression ("9000 / ( (10 + 20) ** 2)")) ; //prints 10
-//console.log (computeExpression("9 + 100 / 2")); //prints 59
-
-// function printAnanas() {
-
-//To use only "A" "S"
-//print "ananas"
-// }
-
-function revers(number) {
+//1
+function getDigitsSum(number) {
+    let sum = 0;
     if (number < 0) {
         number = -number
     }
-    //returns string with digits of given number in reversed order
-    //number may be either positive or negative
-    //consider only integer part fractional
-    //example reverse(1234) should return "43231"
-    //example reverse (-1234) should return "-4321"
+    while (number != 0) {
+        let rem = Math.floor(number % 10);
+        number = Math.floor(number / 10);
+        sum = sum + rem;
+    }
+    return sum;
 }
-console.log(revers(-123))
+console.log("Digits sum: " + getDigitsSum(123.45));
+console.log("Digits sum: " + getDigitsSum(-280.123));
+console.log("Digits sum: " + getDigitsSum(123));
 
-// let n = 10;
-// let count = 0;
-// while (n >= 0) {
-//     n--
-//     count++;
-// }
+//2
+function computeExpression(expressionString) {
+    return eval(expressionString);
 
-//equilty operators
-// let res10 = "123" == 123; //true
-// let res11 = "123" === 123; //false
+}
+console.log("Expression = " + (9000 / ((10 + 20) ** 2)))
+console.log("Expression = " + (9 + 100 / 2))
 
-//string function for HW
-// const str = "aBcdTYg";
-// str = str.toLowerCase(); //"abcdtyg"
+//3
+function printAnanas() {
+    console.log(("A" + +"S" + "A" + "S").toLowerCase())
+}
+printAnanas()
+
+//4
+function reverse(num) {
+    let res = '';
+    if (num < 0) {
+        res = '-';
+        num = -num;
+    }
+    num = Math.floor(num);
+    while (num) {
+        res = res + (num % 10)
+        num = Math.floor(num / 10);
+    }
+    return res;
+}
+console.log("Revers: " + (reverse(-280.123)))
+console.log("Revers: " + (reverse(123)))
+console.log("Revers: " + (reverse(-123)))
+console.log("Revers: " + (reverse(123.45)))
 
