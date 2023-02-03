41348d441f53cd7c12f884c707d713c4b3e2108c
RomanSavushkin <romarsa82@gmail.com>
2023-02-03 12:27:25 +0200

    CW-23

--- src/main.js
+++ src/main.js
@@ -1,17 +1,29 @@
 import { Company, createEmployee } from "./service/company.js";
 import { EmployeeForm } from "./ui/employee-form.js";
-import { getRandomNumber } from "./utils/random.js";
-const MIN_ID = 100000000;
-const MAX_ID = 999999999;
+import { Table } from "./ui/table.js";
+
+const schema = [
+    { columnName: 'Employee ID', fieldName: 'id' },
+    { columnName: 'Name', fieldName: 'name' },
+    { columnName: 'Birth Year', fieldName: 'birthYear' },
+    { columnName: 'Salary (NIS)', fieldName: 'salary' },
+    { columnName: 'Country', fieldName: 'country' },
+    { columnName: 'City', fieldName: 'city' }
+]
 const company = new Company();
 const employeeForm = new EmployeeForm("form-section")
+const tableEmployees = new Table("table-section", "Employees", schema);
 function addEmployee(employeeData) {
-    const id = getRandomNumber(MIN_ID, MAX_ID);
-    const employee = createEmployee(id, employeeData.name,
+
+    const employee = createEmployee(employeeData.name,
         +employeeData.birthYear, +employeeData.salary,
         employeeData.city, employeeData.country);
-    company.addEmployee(employee);
-    console.log(employee)
+    const res = company.addEmployee(employee);
+    if (!res.message) {
+        employeeData.id = res.id;
+        tableEmployees.addRow(employeeData);
+    }
+    return res.message;
+
 }
 employeeForm.addFormHandler(addEmployee)
-
--- src/service/company.js
+++ src/service/company.js
@@ -1,8 +1,9 @@
- 
-/* HW #21 */
+import { employeeConfig } from "../config/employee-config.js";
+import { getRandomNumber } from "../utils/random.js";
+
 // Employee structure and function createEmployee() taken from previous HW
-export function createEmployee(id, name, birthYear, salary, city, country) {
-    return {id, name, birthYear, salary, address: {city, country}}
+export function createEmployee(name, birthYear, salary, city, country) {
+    return { name, birthYear, salary, address: { city, country } }
 }
 export class Company {
     #employees //object key: <id value>, value: reference to Employee object
@@ -10,20 +11,15 @@ export class Company {
         this.#employees = {};
     }
     addEmployee(empl) {
-        //adds empl into #employees object
-        //returns true if added new employee object
-        //returns false if employee with a given id value already exists
-        let res = false;
-        if (!this.#employees[empl.id]) {
-            this.#employees[empl.id] = empl;
-            res = true;
+        const res = checkEmployeeData(empl);
+        const id = this.#getId();
+        if (res === '') {
+            empl.id = id;
+            this.#employees[id] = empl;
         }
-        return res;
+        return { message: res, id: id };
     }
     removeEmployee(id) {
-        //removes employee with a given id from #employees object
-        //returns true if removed
-        //returns false if employee with the id doesn't exist
         let res = false;
         if (this.#employees[id]) {
             res = true;
@@ -33,24 +29,41 @@ export class Company {
     getEmployeesCountry(country) {
         //returns array of employee objects having field "country" equaled to a given country
         return Object.values(this.#employees)
-        .filter(empl => empl.address.country === country);
+            .filter(empl => empl.address.country === country);
     }
     getEmployeesByAge(age) {
-        
+
         const currentYear = new Date().getFullYear();
         return Object.values(this.#employees)
-        .filter(empl => currentYear - empl.birthYear === age);
+            .filter(empl => currentYear - empl.birthYear === age);
     }
     getEmployeesBySalaries(salaryFrom, salaryTo) {
-        //returns array of employee objects with salary in a given closed range [salaryFrom, salaryTo]
-        //if salaryFrom < 0, then get employees with salary less or equal salaryTo
-        //if salaryTo < 0, then get employees with salary greater or equal salaryFrom
-        //if salaryFrom < 0 && salaryTo < 0, then get all employees
         if (salaryTo < 0) {
             salaryTo = Number.MAX_VALUE
         }
         return Object.values(this.#employees)
-        .filter(empl => empl.salary >= salaryFrom && empl.salary <= salaryTo);
-
+            .filter(empl => empl.salary >= salaryFrom && empl.salary <= salaryTo);
     }
+    #getId() {
+        let id = 0;
+        do {
+            id = getRandomNumber(employeeConfig.minId, employeeConfig.maxId + 1);
+        } while (this.#employees[id]);
+        return id;
+    }
+    getAllEmployees() {
+        return Object.values(this.#employees)
+    }
+}
+function checkEmployeeData(employee) {
+    let resStr = '';
+    if (employee.salary < employeeConfig.minSalary ||
+        employee.salary > employeeConfig.maxSalary) {
+        resStr = `salary must be in the range [${employeeConfig.minSalary}-${employeeConfig.maxSalary}]; `
+    }
+    if (employee.birthYear < employeeConfig.minYear ||
+        employee.birthYear > employeeConfig.maxYear) {
+        resStr += `birth year must be in the range [${employeeConfig.minYear}-${employeeConfig.maxYear}]`
+    }
+    return resStr;
 }
